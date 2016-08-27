0951a53a1ddc8fa7400198fe305c9c5355d1c891
caon <894870246@qq.com>
2016-08-27 12:27:44 +0800

    change the path

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /**
  * Created by Administrator on 2016/8/27.
  */
+var fs= require('fs');
 module.exports = function() {
     fis.on('release:end', function() {
         var usedByArr = [];
         if (fis.config.get('usedByNS')) {
             usedByArr = fis.config.get('usedByNS').split(',');
         }
-
-        var rootPath = __dirname.replace(fis.get('namespace'), '');
+        var rootPath = fis.project.getProjectPath().replace(fis.get('namespace'), '');
         for (var i in usedByArr) {
             var path = rootPath + usedByArr[i] + '/fis-conf.js';
             fs.writeFileSync(path, fs.readFileSync(path));
         }
     });
-};
+}();
