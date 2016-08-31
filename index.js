e916a18ccc68cae14ad764510edc999d076e7832
caon <894870246@qq.com>
2016-08-31 17:07:59 +0800

    change the 'release:end' to 'deploy:end'

--- index.js
+++ index.js
@@ -3,16 +3,31 @@
  */
 var fs = require('fs');
 var fisCrossApp = module.exports = {};
-module.exports = function() {
-    fis.on('release:end', function() {
+module.exports = function () {
+    fis.on('deploy:end', function () {
         var usedByArr = [];
         if (fis.config.get('usedByNS')) {
             usedByArr = fis.config.get('usedByNS').split(',');
         }
+        if (usedByArr.length === 0) {
+            return;
+        }
         var rootPath = fis.project.getProjectPath().replace(fis.get('namespace'), '');
+
+        function sleep(milliseconds) {
+            var start = new Date().getTime();
+            for (var i = 0; i < 1e7; i++) {
+                if ((new Date().getTime() - start) > milliseconds) {
+                    break;
+                }
+            }
+        }
+
         for (var i in usedByArr) {
             var path = rootPath + usedByArr[i] + '/fis-conf.js';
             fs.writeFileSync(path, fs.readFileSync(path));
+            console.log(path);
+            // sleep(2000);
         }
     });
 }();
